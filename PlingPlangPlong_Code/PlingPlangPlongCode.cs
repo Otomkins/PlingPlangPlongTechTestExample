857b9af6df6c563b9101b9c9b0b009f6e84ce33e
Oliver Tomkins <otomkins@yahoo.co.uk>
2021-02-09 11:39:36 +0000

    Added initial PlingPlangPlong code

--- PlingPlangPlong_Code/PlingPlangPlongCode.cs
+++ PlingPlangPlong_Code/PlingPlangPlongCode.cs
@@ -5,5 +5,24 @@ namespace PlingPlangPlong_Code
 {
     public class PlingPlangPlongCode
     {
+        public string PlingPlangPlong(int num)
+        {
+            var numFactors = new List<int>();
+            var returnString = "";
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (num % i == 0) numFactors.Add(i);
+            }
+
+            if (numFactors.Contains(3)) returnString += "Pling";
+            if (numFactors.Contains(5)) returnString += "Plang";
+
+            if (numFactors.Contains(7)) returnString += "Plong";
+            else if (!numFactors.Contains(3) && !numFactors.Contains(5)) return num.ToString();
+
+            return returnString; 
+        }
+
     }
 }
