aa1d1187a67c68a052e6ab29cfa9a280fff32e75
Oliver Tomkins <otomkins@yahoo.co.uk>
2021-02-09 12:45:38 +0000

    Added functionality and testing for minus numbers & 0

--- PlingPlangPlong_Code/PlingPlangPlongCode.cs
+++ PlingPlangPlong_Code/PlingPlangPlongCode.cs
@@ -22,18 +22,35 @@ namespace PlingPlangPlong_Code
             var numFactors = new List<int>();
             var returnString = "";
 
-            for (int i = 1; i <= num; i++)
+            if (num < 0)
             {
-                if (num % i == 0) numFactors.Add(i);
+                for (int i = -1; i >= num; i--)
+                {
+                    if (num % i == 0) numFactors.Add(i);
+                }
+                if (numFactors.Contains(-3)) returnString += "Pling";
+                if (numFactors.Contains(-5)) returnString += "Plang";
+
+                if (numFactors.Contains(-7)) returnString += "Plong";
+                else if (!numFactors.Contains(-3) && !numFactors.Contains(-5)) return num.ToString();
             }
+            else if (num == 0)
+            {
+                return "PlingPlangPlong";
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0) numFactors.Add(i);
+                }
+                if (numFactors.Contains(3)) returnString += "Pling";
+                if (numFactors.Contains(5)) returnString += "Plang";
 
-            if (numFactors.Contains(3)) returnString += "Pling";
-            if (numFactors.Contains(5)) returnString += "Plang";
-
-            if (numFactors.Contains(7)) returnString += "Plong";
-            else if (!numFactors.Contains(3) && !numFactors.Contains(5)) return num.ToString();
-
-            return returnString; 
+                if (numFactors.Contains(7)) returnString += "Plong";
+                else if (!numFactors.Contains(3) && !numFactors.Contains(5)) return num.ToString();
+            }
+            return returnString;
         }
 
     }
--- PlingPlangPlong_Tests/PlingPlangPlongTests.cs
+++ PlingPlangPlong_Tests/PlingPlangPlongTests.cs
@@ -25,6 +25,7 @@ namespace PlingPlangPlong_Tests
         }
 
         [TestCase(9, "Pling")]
+        [TestCase(-9, "Pling")]
         public void GivenIHaveANumberWithAFactorOf3_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPling(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -32,6 +33,7 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(10, "Plang")]
+        [TestCase(-10, "Plang")]
         public void GivenIHaveANumberWithAFactorOf5_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlang(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -39,6 +41,7 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(28, "Plong")]
+        [TestCase(-28, "Plong")]
         public void GivenIHaveANumberWithFactorOf7_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlong(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -46,6 +49,7 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(30, "PlingPlang")]
+        [TestCase(-30, "PlingPlang")]
         public void GivenIHaveANumberWithFactorsOf3And5_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlingPlang(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -53,6 +57,7 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(105, "PlingPlangPlong")]
+        [TestCase(-105, "PlingPlangPlong")]
         public void GivenIHaveANumberWithFactorsOf3And5And7_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlingPlangPlong(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -60,6 +65,7 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(42, "PlingPlong")]
+        [TestCase(-42, "PlingPlong")]
         public void GivenIHaveANumberWithFactorsOf3And7_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlingPlong(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -67,6 +73,7 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(35, "PlangPlong")]
+        [TestCase(-35, "PlangPlong")]
         public void GivenIHaveANumberWithFactorsOf5And7_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlangPlong(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
@@ -74,11 +81,20 @@ namespace PlingPlangPlong_Tests
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
         [TestCase(34, "34")]
+        [TestCase(-34, "-34")]
         public void GivenIHaveANumberWithNoFactorsOf3And5And7_WhenIEnterItIntoPlingPlangPlong_ThenTheOriginalNumber(int num, string expected)
         {
             pppCode.PlingPlangPlong(num);
 
             Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
         }
+        [TestCase(0, "PlingPlangPlong")]
+        public void GivenIUse0_WhenIEnterItIntoPlingPlangPlong_ThenItReturnsPlingPlangPlong(int num, string expected)
+        {
+            pppCode.PlingPlangPlong(num);
+
+            Assert.That(pppCode.PlingPlangPlong(num), Is.EqualTo(expected));
+        }
+
     }
 }
